9da5ffb06953982bc649657148ac973e5f865700
한태경 <hanskd12345@gmail.com>
2022-06-29 00:52:04 +0900

    여러개의 font와 assets preload

--- App.js
+++ App.js
@@ -1,13 +1,39 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Asset } from "expo-asset";
 
 export default function App() {
   const [ready, setReady] = useState(false);
+  const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
+  const loadImages = (assets) =>
+    assets.map((asset) => {
+      if (typeof asset === "string") {
+        return Image.prefetch(asset);
+      } else {
+        return Asset.loadAsync(asset);
+      }
+    });
   const onFinish = () => setReady(true);
   const startLoading = async () => {
-    await new Promise((reslove) => setTimeout(reslove, 10000));
+    //font를 preload
+    // await Font.loadAsync(Ionicons.font);
+    // const [{ localUri }] = await Asset.loadAsync(
+    //   require("./wallpaperbetter.jpg")
+    // );
+    // await Image.prefetch("https://en.pimg.jp/047/504/268/1/47504268.jpg");
+
+    //아래처럼하면 여러개 폰트 preload
+    const fonts = loadFonts([Ionicons.font]);
+    const assets = loadImages([
+      require("./wallpaperbetter.jpg"),
+      "https://en.pimg.jp/047/504/268/1/47504268.jpg",
+    ]);
+
+    await Promise.all([...fonts, ...assets]);
   };
   if (!ready) {
     return (

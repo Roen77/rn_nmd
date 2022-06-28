e412121498d3a4e74c09a6232ddfcfd54f9a5e69
한태경 <hanskd12345@gmail.com>
2022-06-28 23:51:44 +0900

    create-react-native-app으로 초기셋팅

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import AppLoading from "expo-app-loading";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [ready, setReady] = useState(false);
+  const onFinish = () => setReady(true);
+  const startLoading = async () => {
+    await new Promise((reslove) => setTimeout(reslove, 10000));
+  };
+  if (!ready) {
+    return (
+      <AppLoading
+        //먼저 시작
+        startAsync={startLoading}
+        //  startAsync 후 호출
+        onFinish={onFinish}
+        onError={console.warn}
+      />
+    );
+  }
+  return null;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });

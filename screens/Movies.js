8cd08740ec482cdf0b0952fb1c6c0418ab9b23af
한태경 <roenh@statnco.com>
2022-06-29 17:17:22 +0900

    styled component 적용1

--- screens/Movies.js
+++ screens/Movies.js
@@ -1,14 +1,25 @@
 import React from "react";
 import { Text, TouchableOpacity, View } from "react-native";
+import styled from "styled-components/native";
+
+const Btn = styled.TouchableOpacity`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Title = styled.Text`
+    color: blue;
+`;
 
 function Movies({ navigation: { navigate } }) {
     return (
-        <TouchableOpacity
+        <Btn
             style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
             onPress={() => navigate("Stack", { screen: "Three" })}
         >
-            <Text>movi</Text>
-        </TouchableOpacity>
+            <Title>movi</Title>
+        </Btn>
     );
 }
 

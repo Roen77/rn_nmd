75ecdde9cc8d9cf483fa47bd57462e4c4c46d67b
한태경 <roenh@statnco.com>
2022-06-29 10:35:21 +0900

    다크모드 감지

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -4,46 +4,46 @@ import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
 import { Text, View } from "react-native";
+import { useColorScheme } from "react-native";
+import { BLACK_COLOR, YELLOW_COLOR } from "../colors";
 
 const Tab = createBottomTabNavigator();
 
-const Tabs = () => (
-    //tab 전체에 옵션을 주고싶다면 Nacigator에 screenOptions 각각주고싶다면 각 screen에 Options 사용
-    <Tab.Navigator
-        screenOptions={{
-            tabBarStyle: { backgroundColor: "tomato" },
-            tabBarLabelStyle: {
-                backgroundColor: "lightblue",
-            },
-            tabBarLabelPosition: "beside-icon",
-            tabBarActiveTintColor: "red",
-            tabBarInactiveTintColor: "purple",
-        }}
-    >
-        <Tab.Screen
-            name="Movies"
-            component={Movies}
-            options={{
-                headerTitleStyle: { color: "blue" },
-                headerRight: () => (
-                    <View>
-                        <Text>hrll</Text>
-                    </View>
-                ),
-            }}
-        />
-        <Tab.Screen
-            name="TV"
-            component={Tv}
-            options={{
-                tabBarLabelStyle: {
-                    backgroundColor: "pink",
+const Tabs = () => {
+    const isDark = useColorScheme() === "dark";
+
+    return (
+        //tab 전체에 옵션을 주고싶다면 Nacigator에 screenOptions 각각주고싶다면 각 screen에 Options 사용
+        <Tab.Navigator
+            screenOptions={{
+                tabBarStyle: {
+                    backgroundColor: isDark ? BLACK_COLOR : "white",
                 },
-                tabBarBadge: 5,
+                tabBarLabelPosition: "beside-icon",
+                tabBarActiveTintColor: isDark ? YELLOW_COLOR : "#1e272e",
+                tabBarInactiveTintColor: isDark ? "#d2dae2" : "#808e9b",
+                headerStyle: {
+                    backgroundColor: isDark ? BLACK_COLOR : "white",
+                },
+                headerTitleStyle: {
+                    color: isDark ? "white" : BLACK_COLOR,
+                },
             }}
-        />
-        <Tab.Screen name="Search" component={Search} />
-    </Tab.Navigator>
-);
+        >
+            <Tab.Screen name="Movies" component={Movies} />
+            <Tab.Screen
+                name="TV"
+                component={Tv}
+                options={{
+                    tabBarLabelStyle: {
+                        backgroundColor: "pink",
+                    },
+                    tabBarBadge: 5,
+                }}
+            />
+            <Tab.Screen name="Search" component={Search} />
+        </Tab.Navigator>
+    );
+};
 
 export default Tabs;
